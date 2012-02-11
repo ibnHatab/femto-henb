4535e166f57090d7bb735c244b790dca13363d39
ibnHatab <lib.aca55a@gmail.com>
2012-02-11 11:27:19 +0200

    make adaptation to .dir-locals.el

--- src/testing/test_runner.c
+++ src/testing/test_runner.c
@@ -1,12 +1,13 @@
 #include <linux/module.h>
 #include <embUnit/embUnit.h>
 
-MODULE_DESCRIPTION("embunit sample for linux kernel");
-MODULE_AUTHOR("Mikhail Gruzdev <michail.gruzdev@gmail.com>");
+MODULE_DESCRIPTION("embunit runner for linux kernel");
 MODULE_LICENSE("GPL");
 
+/*embunit:extern=+ */
 extern TestRef CounterTest_tests(TestCaller *test);
 extern TestRef PersonTest_tests(TestCaller *test);
+/*embunit:extern=- */
 
 void stdimpl_print(const char *string)
 {
@@ -18,8 +19,10 @@ static int __init test_runner_init(void)
 	static TestCaller test;
 
 	TestRunner_start();
+	/*embunit:run=+ */
 	TestRunner_runTest(CounterTest_tests(&test));
 	TestRunner_runTest(PersonTest_tests(&test));
+	/*embunit:run=- */
 	TestRunner_end();
 
 	return 0;
